c58ea0424160176c90783f7a5d9fe2d99be942da
EmanuelCN <emanuelghub@gmail.com>
2022-03-16 20:11:10 +0100

    kernel: power: Checkout to msm-5.4
    *From LA.UM.9.14.r1-19300-LAHAINA.QSSI13.0
    
    Signed-off-by: EmanuelCN <emanuelghub@gmail.com>

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -57,6 +57,19 @@ void unlock_system_sleep(void)
 }
 EXPORT_SYMBOL_GPL(unlock_system_sleep);
 
+void ksys_sync_helper(void)
+{
+	ktime_t start;
+	long elapsed_msecs;
+
+	start = ktime_get();
+	ksys_sync();
+	elapsed_msecs = ktime_to_ms(ktime_sub(ktime_get(), start));
+	pr_info("Filesystems sync: %ld.%03ld seconds\n",
+		elapsed_msecs / MSEC_PER_SEC, elapsed_msecs % MSEC_PER_SEC);
+}
+EXPORT_SYMBOL_GPL(ksys_sync_helper);
+
 /* Routines for PM-transition notifications */
 
 static BLOCKING_NOTIFIER_HEAD(pm_chain_head);
