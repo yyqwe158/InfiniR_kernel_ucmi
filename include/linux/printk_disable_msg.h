05fcca9b11887cc9aaf78ba0d95911d60955d81a
repinger <devel@repinger.my.id>
2023-12-22 23:13:10 +0700

    printk_disable_msg: Silence ratelimited macros too
    Signed-off-by: repinger <devel@repinger.my.id>

--- include/linux/printk_disable_msg.h
+++ include/linux/printk_disable_msg.h
@@ -6,6 +6,11 @@
 #undef pr_warn
 #undef pr_dbg
 #undef pr_debug
+#undef pr_info_ratelimited
+#undef pr_err_ratelimited
+#undef pr_warn_ratelimited
+#undef pr_dbg_ratelimited
+#undef pr_debug_ratelimited
 
 #define printk(fmt, ...)
 #define pr_info(fmt, ...)
@@ -13,11 +18,19 @@
 #define pr_warn(fmt, ...)
 #define pr_dbg(fmt, ...)
 #define pr_debug(fmt, ...)
+#define pr_info_ratelimited(fmt, ...)
+#define pr_err_ratelimited(fmt, ...)
+#define pr_warn_ratelimited(fmt, ...)
+#define pr_dbg_ratelimited(fmt, ...)
+#define pr_debug_ratelimited(fmt, ...)
 
 #undef dev_info
 #undef dev_dbg
 #undef dev_err
 #undef dev_warn
+#undef dev_info_ratelimited
+#undef dev_dbg_ratelimited
+#undef dev_warn_ratelimited
 #undef dev_err_ratelimited
 
 #define dev_info(dev, fmt, ...)
@@ -25,6 +38,9 @@
 #define dev_err(dev, fmt, ...)
 #define dev_warn(dev, fmt, ...)
 #define dev_err_ratelimited(dev, fmt, ...)
+#define dev_info_ratelimited(dev, fmt, ...)
+#define dev_dbg_ratelimited(dev, fmt, ...)
+#define dev_warn_ratelimited(dev, fmt, ...)
 
 #undef qg_dbg
 #undef smblib_dbg
